6eca9252427cd3b92901e9d3d0f6628b85e6f8c6
shawon <shawon.softeng@gmail.com>
2023-04-25 10:55:41 +0600

    second

---
+++ json-jsx-state/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div>
+            <h2> Hello JSX </h2>
+            <h3> Hello hello</h3>
+        </div>
+    );
+};
+
+export default App;
